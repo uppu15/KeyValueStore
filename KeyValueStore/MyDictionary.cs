0b92b9e69ad3f59d76f13da6ccda6ab3535a0ea6
Jehyun Park <uppu1507@gmail.com>
2019-09-14 22:13:16 -0700

    Add project files.

---
+++ KeyValueStore/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyValueStore
+{
+    public struct KeyValue
+    {
+        public KeyValue(string key, object value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+        public readonly string Key;
+        public readonly object Value;
+    }
+
+    public class MyDictionary
+    {
+        KeyValue[] structArray = new KeyValue[5];
+        int count = 0;
+        public object this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < structArray.Length; i++)
+                {
+                    if (structArray[i].Key == index)
+                        return structArray[i].Value;
+                }
+                throw new KeyNotFoundException(index);
+            }
+            set
+            {
+                for (int i = 0; i < structArray.Length; i++)
+                {
+                    if (structArray[i].Key == index)
+                    {
+                        structArray[i] = new KeyValue(index, value);
+                        return;
+                    }
+                }
+                for (int i = 0; i < structArray.Length; i++)
+                {
+                    if (structArray[i].Key == null)
+                    {
+                        structArray[i] = new KeyValue(index, value);
+                        count++;
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ KeyValueStore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using KeyValueStore;
+
+
+class Program
+{
+    static void Main()
+    {
+        var d = new MyDictionary();
+        try
+        {
+            Console.WriteLine(d["Cats"]);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        d["Cats"] = 42;
+        d["Dogs"] = 17;
+        Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+    }
+}
+
